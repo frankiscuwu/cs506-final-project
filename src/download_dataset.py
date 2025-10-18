4f26026659eb420f6d4b6e1eef56cb58c7552eeb
Frank Yang <frankyang009@gmail.com>
2025-10-18 13:57:30 -0400

    install now works

--- src/download_dataset.py
+++ src/download_dataset.py
@@ -6,20 +6,17 @@ import os
 kagglehub.login()
 
 path = kagglehub.dataset_download("frankiscoo/all-subset")
-print("Path to downloaded file:", path)
-
-dest_folder = "data/raw/ALL"
-nested_folder = os.path.join(path, "datasets/frankiscoo/all-subset/versions/1")
+dest_folder = "data/raw"
 
 # Move all files from nested_folder to dest_folder
-for item in os.listdir(nested_folder):
-    s = os.path.join(nested_folder, item)
+for item in os.listdir(path):
+    s = os.path.join(path, item)
     d = os.path.join(dest_folder, item)
     if os.path.isdir(s):
         shutil.move(s, d)
     else:
         shutil.move(s, d)
 
-shutil.rmtree(os.path.join(path, "datasets"))
+shutil.rmtree("data/raw/ALL/datasets")
 
 print(f"Dataset installed at {dest_folder}")
